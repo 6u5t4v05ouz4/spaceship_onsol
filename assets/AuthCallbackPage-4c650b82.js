f96f883d17bcba43da7c76969eb46a377e772cb7
6u5t4v05ouz4 <n4r1g4@proton.me>
2025-10-21 01:08:49 +0000

    Deploying to gh-pages from @ 6u5t4v05ouz4/spaceship_onsol@333b63e1b69fda74152d08d7cdaf9d890d027dc0 🚀

--- assets/AuthCallbackPage-4c650b82.js
+++ assets/AuthCallbackPage-4c650b82.js
@@ -1,4 +1,4 @@
-import{g as i,h as s,n as c}from"./main-c829573f.js";import"./phaser-aaa04cbd.js";class p{constructor(){this.name="AuthCallbackPage",this.isProcessing=!1}render(){const e=document.createElement("div");return e.className="auth-callback-page",e.innerHTML=`
+import{g as i,h as s,n as c}from"./main-fd77ee0d.js";import"./phaser-aaa04cbd.js";class p{constructor(){this.name="AuthCallbackPage",this.isProcessing=!1}render(){const e=document.createElement("div");return e.className="auth-callback-page",e.innerHTML=`
       <div class="background-primary"></div>
       <div class="stars-background"></div>
       
